bcac89a78c74bf989c6ff8f0ec60b58310ce4fe0
Arik Chakma <arikchangma@gmail.com>
2021-09-07 17:19:25 +0600

    Added useOnScreen hook

---
+++ src/hooks/useOnScreen.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+function useOnScreen(ref, rootMargin = '0px') {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      { rootMargin }
+    );
+
+    const currentElement = ref?.current;
+    if (currentElement) observer.observe(currentElement);
+
+    return () => observer.unobserve(currentElement);
+  });
+
+  return isVisible;
+}
+
+export default useOnScreen;
