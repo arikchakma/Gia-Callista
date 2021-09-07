8496e2659f97ce5554f3527bc5842904fefe22e8
Arik Chakma <arikchangma@gmail.com>
2021-09-07 18:01:08 +0600

    updated w/ bunch of hooks

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import { getLocoScroll } from './src/hooks/getLocoScroll';
 import { getRandomNum, getRandomRGB } from './src/utils/getRandom';
 import getViewportSizes from './src/utils/getViewporSizes';
 import logger from './src/utils/logger';
+import useMousePosition from './src/hooks/useMousePosition';
+import useOnScreen from './src/hooks/useOnScreen';
+import getCoffee from './src/utils/getCoffee';
 
 const Gia = {
 	getLocoScroll,
@@ -9,9 +12,12 @@ const Gia = {
 	getViewportSizes,
 	getRandomNum,
 	getRandomRGB,
+	useMousePosition,
+	useOnScreen,
+	getCoffee
 };
 
 /**
- * @return Gia Object
+ * @returns Gia Object
  */
 module.exports = Gia;
